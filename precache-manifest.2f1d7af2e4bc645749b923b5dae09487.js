8d18c2fbf5d6ae131b071510c23b313f57359d60
Anand Kumar <anandgfx@gmail.com>
2020-07-13 00:55:04 +0100

    Updates

---
+++ precache-manifest.2f1d7af2e4bc645749b923b5dae09487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49e368d7b7c1da1530c6223f3a99e52",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "099954fa4db53744ee1f",
+    "url": "/reactTest/static/css/main.c7a6d0ac.chunk.css"
+  },
+  {
+    "revision": "ded62d78b99af8bd54a9",
+    "url": "/reactTest/static/js/2.9663ea45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTest/static/js/2.9663ea45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099954fa4db53744ee1f",
+    "url": "/reactTest/static/js/main.9cee4ca5.chunk.js"
+  },
+  {
+    "revision": "10fbcb51d257d0ce7ee7",
+    "url": "/reactTest/static/js/runtime-main.6c88e5f7.js"
+  }
+]);
